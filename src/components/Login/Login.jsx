b22d5747acc521201bc576389ef8e2b449c5686c
Your Name <you@example.com>
2022-12-04 18:56:12 +0300

    aboba

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -8,27 +8,27 @@ const Login = () => {
             <p className={style.vkloginHeaderTitle}>ПИВАНДОПАЛУ</p>
 
             <div className={style.loginForm}>
-                <div class="container">
-                    <div class="screen">
-                        <div class="screen__content">
+                <div className="container">
+                    <div className="screen">
+                        <div className="screen__content">
                         <h1>Заходи</h1>
-                            <form class="login">
-                                <div class="login__field">
-                                    <input type="text" class="login__input" placeholder="User name" />
+                            <form className="login">
+                                <div className="login__field">
+                                    <input type="text" className="login__input" placeholder="User name" />
                                 </div>
-                                <div class="login__field">
-                                    <input type="password" class="login__input" placeholder="Пароль" />
+                                <div className="login__field">
+                                    <input type="password" className="login__input" placeholder="Пароль" />
                                 </div>
-                                <button class="button login__submit">
-                                    <span class="button__text">Зайти</span>
+                                <button className="button login__submit">
+                                    <span className="button__text">Зайти</span>
                                 </button>
                             </form>
                         </div>
-                        <div class="screen__background">
-                            <span class="screen__background__shape screen__background__shape4"></span>
-                            <span class="screen__background__shape screen__background__shape3"></span>
-                            <span class="screen__background__shape screen__background__shape2"></span>
-                            <span class="screen__background__shape screen__background__shape1"></span>
+                        <div className="screen__background">
+                            <span className="screen__background__shape screen__background__shape4"></span>
+                            <span className="screen__background__shape screen__background__shape3"></span>
+                            <span className="screen__background__shape screen__background__shape2"></span>
+                            <span className="screen__background__shape screen__background__shape1"></span>
                         </div>
                     </div>
                 </div>
