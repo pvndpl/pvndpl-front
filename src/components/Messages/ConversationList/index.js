ba7a75e283c2927f6a24dc1561be062296942c10
MaxZhukoff <maksus298@gmail.com>
2022-12-16 22:13:11 +0300

    aboba4

--- src/components/Messages/ConversationList/index.js
+++ src/components/Messages/ConversationList/index.js
@@ -3,7 +3,8 @@ import ConversationSearch from '../ConversationSearch';
 import ConversationListItem from '../ConversationListItem';
 import Toolbar from '../Toolbar';
 import ToolbarButton from '../ToolbarButton';
-import axios from 'axios';
+import axios from '../../../redux/axios';
+import Cookies from 'js-cookie';
 
 import './ConversationList.css';
 
@@ -14,12 +15,18 @@ export default function ConversationList(props) {
   },[])
 
  const getConversations = () => {
-    axios.get('https://randomuser.me/api/?results=20').then(response => {
-        let newConversations = response.data.results.map(result => {
+     axios.get(
+         '/chats',
+         {
+             headers: { Authorization: "Bearer ".concat(Cookies.get('JWT')) }
+         }
+     ).then(response => {
+        let newConversations = response.data.map(result => {
           return {
-            photo: result.picture.large,
-            name: `${result.name.first} ${result.name.last}`,
-            text: 'Hello world! This is a long message that needs to be truncated.'
+              photo: "https://upload.wikimedia.org/wikipedia/commons/thumb/4/4e/Macaca_nigra_self-portrait_large.jpg/220px-Macaca_nigra_self-portrait_large.jpg",
+              name: `${result.userFirstName} ${result.userLastName}`,
+              chatId: result.chatId,
+              userId: result.userId
           };
         });
         setConversations([...conversations, ...newConversations])
--- src/components/Messages/ConversationListItem/index.js
+++ src/components/Messages/ConversationListItem/index.js
@@ -2,20 +2,32 @@ import React, {useEffect} from 'react';
 import shave from 'shave';
 
 import './ConversationListItem.css';
+import axios from "../../../redux/axios";
 
 export default function ConversationListItem(props) {
   useEffect(() => {
     shave('.conversation-snippet', 20);
   })
 
-    const { photo, name, text } = props.data;
+    // const showDialog = async (e) => {
+    //     e.preventDefault();
+    //
+    //     await axios.post(
+    //         '/authenticate',
+    //         JSON.stringify({"username": username, "password": password}),
+    //         {
+    //             headers: {"Content-Type": "application/json"}
+    //         }
+    //     ).then((response) => document.cookie = "JWT=".concat(response.data.token)).catch(console.log)
+    // }
+
+    const { photo, name, chatId, userId } = props.data;
 
     return (
-      <div className="conversation-list-item">
+      <div className="conversation-list-item" id={chatId}>
         <img className="conversation-photo" src={photo} alt="conversation" />
         <div className="conversation-info">
           <h1 className="conversation-title">{ name }</h1>
-          <p className="conversation-snippet">{ text }</p>
         </div>
       </div>
     );
