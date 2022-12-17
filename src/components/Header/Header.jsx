f0cbde3d38272e87a6fa4a2200fc54fe9f5b1f1a
MaxZhukoff <maksus298@gmail.com>
2022-12-17 19:35:46 +0300

    fix exit

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -25,9 +25,17 @@ const Header = ({ updateActive }) => {
     }
 
     getConversations()
-    const activateLasers = () =>{
-        Cookies.remove('JWT', { path: '' });
+    const activateLasers = () => {
+        var cookies = document.cookie.split(";");
+        for (var i = 0; i < cookies.length; i++) {
+            var cookie = cookies[i];
+            var eqPos = cookie.indexOf("=");
+            var name = eqPos > -1 ? cookie.substr(0, eqPos) : cookie;
+            document.cookie = name + "=;expires=Thu, 01 Jan 1970 00:00:00 GMT;";
+            document.cookie = name + '=; path=/; expires=Thu, 01 Jan 1970 00:00:01 GMT;';
+        }
     }
+
     if (token) {
         console.log(username)
         return (
